4181382f65940a7a54c73e7fa8afb25295e4bade
Akhilesh <akhilesh.mishra@theschoolprep.com>
2026-03-09 20:28:45 +0530

    writing auth-middleware

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { adminAuth, userAuth } = require('./middlewares/auth');
 
 // create a web server application
 const PORT_NO = 7777;
@@ -6,27 +7,25 @@ const app = express();
 
 // request handlers this server will responds
 // one route can handle mutiple request handlers, and using next() given by express, we can move to next request handler
-app.use(
-  '/test',
-  [
-    (req, res, next) => {
-      console.log('First Request Handler');
-      next();
-    },
-    (req, res, next) => {
-      console.log('Second Request Handler');
-      //   res.send('Hello form DEV Tinder');
-      next();
-    },
-  ],
-  (req, res, next) => {
-    console.log('Third Request Handler');
-    res.send('Third Response');
-  },
-  (req, res, next) => {
-    console.log('forth Request Handler');
-  },
-);
+// app.use()  → prefix matching
+// app.get()  → exact route matching
+// Express executes routes top → bottom
+
+// here use is used for handling middleware because it will check all http method with prefix matching of /admin, /user
+app.use('/admin', adminAuth);
+
+app.get('/admin/getAllData', (req, res) => {
+  console.log('control coming');
+  res.send('Admin data sent');
+});
+
+app.get('/user/getAllData', userAuth, (req, res) => {
+  res.send('User data sent');
+});
+
+app.get('/user/login', (req, res) => {
+  res.send('Login Successfully');
+});
 
 // listen this server on port no 7777
 app.listen(PORT_NO, () => {
