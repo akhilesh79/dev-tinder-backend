7c73a6dc997c43b5aad8a96ca2ef3b1fb6ccb359
Akhilesh <akhilesh.mishra@theschoolprep.com>
2026-03-09 18:53:40 +0530

    multiple request handlers for same route and understood error of Cannot set headers after they are sent to the client meaning

--- src/app.js
+++ src/app.js
@@ -5,17 +5,28 @@ const PORT_NO = 7777;
 const app = express();
 
 // request handlers this server will responds
-app.use('/hello', (req, res) => {
-  res.send('Hello Hello Hello!!');
-});
-
-app.use('/test', (req, res) => {
-  res.send('Hello form DEV Tinder');
-});
-
-app.use('/', (req, res) => {
-  res.send('Namaster To DEV Tinder Node App');
-});
+// one route can handle mutiple request handlers, and using next() given by express, we can move to next request handler
+app.use(
+  '/test',
+  [
+    (req, res, next) => {
+      console.log('First Request Handler');
+      next();
+    },
+    (req, res, next) => {
+      console.log('Second Request Handler');
+      //   res.send('Hello form DEV Tinder');
+      next();
+    },
+  ],
+  (req, res, next) => {
+    console.log('Third Request Handler');
+    res.send('Third Response');
+  },
+  (req, res, next) => {
+    console.log('forth Request Handler');
+  },
+);
 
 // listen this server on port no 7777
 app.listen(PORT_NO, () => {
