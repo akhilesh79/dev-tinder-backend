b205d2dfe5b910f7f8648fd08181a7ab0ffb6cee
Akhilesh <akhilesh.mishra@theschoolprep.com>
2026-03-10 13:18:59 +0530

    schema validations

--- src/models/user.js
+++ src/models/user.js
@@ -1,31 +1,55 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const userSchema = new Schema({
-  firstName: {
-    type: String,
-    required: true,
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 3,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    emailId: {
+      type: String,
+      unique: true,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+    profileImage: {
+      type: String,
+      default:
+        'https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-avatar-image-for-profile-png-image_13001882.png',
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!['male', 'female', 'others'].includes(value)) {
+          throw new Error('Gender is not valid');
+        }
+      },
+    },
+    about: {
+      type: String,
+      default: 'This is default description about a developer',
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  emailId: {
-    type: String,
-  },
-  profileImage: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  { timestamps: true },
+);
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
