7dbd702b6c2a1927ca72bf6649985a5a4e97a80c
Akhilesh Kumar Mishra <makmishra.99@gmail.com>
2026-03-13 01:31:14 +0530

    feeds api with pagination using skip and limit of mongodb

--- src/api/user/routes.js
+++ src/api/user/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { CustomAPIError } = require('../../utils/customError');
 const ConnectionRequest = require('../../models/connectionRequest');
+const User = require('../../models/user');
 const router = express.Router();
 
 router.get('/requests/recieved', async (req, res) => {
@@ -61,6 +62,93 @@ router.get('/connections', async (req, res) => {
     });
   } catch (error) {
     throw new CustomAPIError('requests-recieved', error.message, error.statusCode || 500);
+  }
+});
+
+router.get('/feeds', async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const { page = 1, limit = 10 } = req.query;
+    const skip = (page - 1) * limit;
+
+    const aggregationQuery = [
+      {
+        $match: {
+          _id: {
+            $ne: loggedInUser._id,
+          },
+        },
+      },
+      {
+        $lookup: {
+          from: 'connectionrequests',
+          let: {
+            loggedInUserId: loggedInUser._id,
+          },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $or: [{ $eq: ['$fromUserId', '$$loggedInUserId'] }, { $eq: ['$toUserId', '$$loggedInUserId'] }],
+                },
+              },
+            },
+            {
+              $project: {
+                fromUserId: 1,
+                toUserId: 1,
+              },
+            },
+          ],
+          as: 'requests',
+        },
+      },
+      {
+        $addFields: {
+          alreadyExistsConnectionRequest: {
+            $setUnion: ['$requests.fromUserId', '$requests.toUserId'],
+          },
+        },
+      },
+      {
+        $match: {
+          $expr: {
+            $not: {
+              $in: ['$_id', '$alreadyExistsConnectionRequest'],
+            },
+          },
+        },
+      },
+      {
+        $project: {
+          firstName: 1,
+          lastName: 1,
+          profileImage: 1,
+          age: 1,
+          gender: 1,
+          about: 1,
+          skills: 1,
+        },
+      },
+    ];
+
+    const totalDocs = (await User.aggregate(aggregationQuery)).length;
+    const userFeeds = await User.aggregate([...aggregationQuery, { $skip: skip }, { $limit: limit }]);
+
+    const totalPages = Math.ceil(totalDocs / limit);
+
+    res.json({
+      message: 'Feeds fetched successfully',
+      docs: userFeeds,
+      page: page,
+      limit: limit,
+      totalDocs,
+      totalPages,
+      hasNextPage: page < totalPages,
+      hasPrevPage: page > 1,
+    });
+  } catch (error) {
+    throw new CustomAPIError('feed', error.message, error.statusCode || 500);
   }
 });
 
