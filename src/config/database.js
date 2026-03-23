159e6fd3603bbe3e4f25bdfbf75f3f85c1692499
Akhilesh Kumar Mishra <makmishra.99@gmail.com>
2026-03-23 20:24:31 +0530

    update MongoDB connection string and remove unnecessary line in app.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const MONGODB_URL = 'mongodb+srv://makmishra99:Akhilesh99@cluster0.kszrwuf.mongodb.net/devTinder?appName=Cluster0';
+const MONGODB_URL =
+  'mongodb+srv://makmishra99:S0YF9cyhpKicVsgH@cluster0.kszrwuf.mongodb.net/devTinder?appName=Cluster0';
 const connectDB = async () => {
   return mongoose.connect(MONGODB_URL);
 };
